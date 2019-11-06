5436ee72c5ff3ff9a8da56e7ef4a5b0f7f6bc92c
Michael P <mikepierredotcom@gmail.com>
2019-11-06 14:45:11 -0500

    Initial commit

---
+++ Controller/Hotels.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Controller.Hotels
+{
+    public class Hotels
+    {
+        public void book(){
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Controller.Hotels;
+
+namespace travel_panel_api_search_sdk_net
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
