e010787f0b1d8fb905300d66533acf192d9e0d13
Michael P <mikepierredotcom@gmail.com>
2019-11-07 10:48:45 -0500

    Added Reports, and Transfers POCOs

---
+++ POCO/Reports/GetAllReservations.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace POCO.Reports
+{
+    public class GetAllReservations 
+    {
+
+        public string Authorization { get; set; }
+        public string PRODUCT_ID { get; set; }
+        public string PRODUCT_NAME { get; set; }
+        public string SERVICE { get; set; }
+        public string SESS_ID { get; set; }
+        public string GRAND_TOTAL { get; set; }
+        public string START_LOCATION	 { get; set; }
+        public string END_LOCATION { get; set; }
+        public string FIRST_NAME { get; set; }
+        public string LAST_NAME { get; set; }
+        public string RESERVATION_DATE { get; set; }
+        public string IS_CANCELLED { get; set; }
+        public string PROVIDER { get; set; }
+        public string PRODUCT_TYPE { get; set; }
+        public string ADULT_QUANTITY { get; set; }
+        public string CHILD_QUANTITY { get; set; }
+        public string START_DATE { get; set; }
+        public string END_DATE { get; set; }
+        public string CAR_RENTAL_RATE_CODE { get; set; }
+    }
+
+}
---
+++ POCO/Transfers/Book.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace POCO.Transfers
+{
+    public class Book
+ 
+    {
+        public string pickup_location  { get; set; }
+        public string pickup_location  { get; set; }
+        public string check_in  { get; set; }
+        public string pickup_time  { get; set; }
+        public string dropp_off_location  { get; set; }
+        public string car_type  { get; set; }
+        public string rate_code  { get; set; }
+        public string request_type  { get; set; }
+        public string REFERENCE_ID  { get; set; }
+        public string REFERENCE_TYPE  { get; set; }
+        public string COMPANY  { get; set; }
+        public string COMPANY_LOGO  { get; set; }
+        public string IP_ADDRESS  { get; set; }
+        public string CAR_NAME  { get; set; }
+        public string CAR_CODE  { get; set; }
+        public string PICTURE_URL  { get; set; }
+        public string DOOR_COUNT  { get; set; }
+        public string PASSENGER_QUANTITY  { get; set; }
+        public string BAGGAGE_QUANTITY  { get; set; }
+        public string VENDOR_CAR_TYPE  { get; set; }
+        public string ESTIMATED_TOTAL_AMOUNT  { get; set; }
+        public string CURRENCY_CODE  { get; set; }
+        public string PICKUP_LOCATION_CODE  { get; set; }
+        public string PICKUP_LOCATION  { get; set; }
+        public string DROPOFF_LOCATION_CODE  { get; set; }
+        public string DROPOFF_LOCATION  { get; set; }
+        public string PICKUP_DATE_TIME  { get; set; }
+        public string RETURN_DATE_TIME  { get; set; }
+        public string RATE_CODE  { get; set; }
+        public string RATE_TOTAL_AMOUNT  { get; set; }
+        public string FEES_JSON  { get; set; }
+    }
+
+}
